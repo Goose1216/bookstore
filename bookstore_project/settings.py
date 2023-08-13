2017d48f2aea4eeedd6f3a3016bd57995f4e7225
Goose1216 <leha.porublev@mail.ru>
2023-08-13 19:06:33 +0300

    test

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DJANGO_DEBUG', default=False)
 
-ALLOWED_HOSTS = ['goose1216.pythonanywhere.com.', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['amvera-goose1216-run-bookstore', 'localhost', '127.0.0.1']
 
 # Application definition
 
